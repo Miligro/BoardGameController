1e331d6221619d083fcc05b3db83215646ea57ef
Miligro <milo20054@gmail.com>
2022-05-26 21:17:29 +0200

    Before start SQLite

--- app/src/main/java/com/example/bgc/MainScreen.kt
+++ app/src/main/java/com/example/bgc/MainScreen.kt
@@ -1,11 +1,13 @@
 package com.example.bgc
 
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.bgc.databinding.FragmentMainScreenBinding
 
@@ -42,8 +44,7 @@ class MainScreen : Fragment() {
             findNavController().navigate(R.id.action_mainScreen_to_synchronization)
         }
         binding.clearDataBtn.setOnClickListener {
-            Toast.makeText(activity, "Czyszczenie danych", Toast.LENGTH_LONG).show()
-//            findNavController().navigate(R.id.action_mainScreen_to_gamesAddOns)
+            clearDataBase()
         }
     }
 
@@ -52,5 +53,18 @@ class MainScreen : Fragment() {
         _binding = null
     }
 
+    private fun clearDataBase(){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireActivity())
+        builder.setCancelable(true)
+        builder.setTitle("Potwierdzenie")
+        builder.setMessage("Czy na pewno chcesz wyczyścić dane?")
+        builder.setPositiveButton("Zatwierdz",
+            DialogInterface.OnClickListener { dialog, which ->
+                Toast.makeText(activity, "Zatwierdzono", Toast.LENGTH_SHORT).show()})
+        builder.setNegativeButton("Anuluj",
+            DialogInterface.OnClickListener { dialog, which -> })
 
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
 }
