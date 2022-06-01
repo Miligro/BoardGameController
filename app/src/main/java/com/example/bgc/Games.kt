49bac14a25333a18dcbf92236f8f039f7fb3c888
Miligro <milo20054@gmail.com>
2022-06-01 21:39:45 +0200

    Games list

--- app/src/main/java/com/example/bgc/Games.kt
+++ app/src/main/java/com/example/bgc/Games.kt
@@ -1,38 +1,46 @@
 package com.example.bgc
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.bgc.databinding.FragmentGamesBinding
+
 
 class Games : Fragment() {
     private var _binding: FragmentGamesBinding? = null
     private lateinit var dbHandler: MyDBHandler
     private val binding get() = _binding!!
 
+    private lateinit var adapter: GamesListAdapter
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val view = inflater.inflate(R.layout.fragment_games, container, false)
-
-        val adapter = GamesListAdapter()
-        adapter.setData(games)
-
         _binding = FragmentGamesBinding.inflate(inflater, container, false)
-        return binding.root
 
+        recyclerView = binding.recyclerView;
+        adapter = GamesListAdapter()
+
+        val llm = LinearLayoutManager(activity)
+        llm.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = llm
+        recyclerView.adapter = adapter
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         dbHandler = MyDBHandler(requireActivity(), null, null, 1)
         val gameListArray: ArrayList<GameAddOn> = dbHandler.getGames()
         val sizeGames = gameListArray.size
+        adapter.setData(gameListArray)
         Toast.makeText(activity, "Ilość gier: $sizeGames", Toast.LENGTH_LONG).show()
         super.onViewCreated(view, savedInstanceState)
     }
--- app/src/main/java/com/example/bgc/GamesListAdapter.kt
+++ app/src/main/java/com/example/bgc/GamesListAdapter.kt
@@ -1,15 +1,25 @@
 package com.example.bgc
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 class GamesListAdapter: RecyclerView.Adapter<GamesListAdapter.MyViewHolder>() {
 
     private var gamesList = emptyList<GameAddOn>()
 
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){}
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val lpText: TextView = itemView.findViewById(R.id.lpText)
+        val titleText: TextView = itemView.findViewById(R.id.titleText)
+        val releaseYearText: TextView = itemView.findViewById(R.id.releaseText)
+        val rankingText: TextView = itemView.findViewById(R.id.rankingText)
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row, parent, false))
@@ -17,14 +27,19 @@ class GamesListAdapter: RecyclerView.Adapter<GamesListAdapter.MyViewHolder>() {
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentItem = gamesList[position]
-
+        val lp = position + 1
+        holder.lpText.text = lp.toString()
+        holder.titleText.text = currentItem.title
+        holder.releaseYearText.text = currentItem.releaseYear.toString()
+        holder.rankingText.text = currentItem.ranking.toString()
+        Picasso.get().load(currentItem.img).into(holder.imageView);
     }
 
     override fun getItemCount(): Int {
         return gamesList.size
-
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     fun setData(games: List<GameAddOn>){
         this.gamesList = games
         notifyDataSetChanged()
